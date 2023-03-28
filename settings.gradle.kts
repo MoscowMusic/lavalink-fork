a0743d0288dfdbfc8d18e1686174f418aee8af29
Kirill Blagochev <moscowcityxd@list.ru>
2023-03-28 17:11:59 +0300

    Update lavaplayer to 1.4.11

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -38,7 +38,7 @@ fun VersionCatalogBuilder.spring() {
 }
 
 fun VersionCatalogBuilder.voice() {
-    library("lavaplayer",            "com.github.MoscowMusic.lavaplayer-fork", "lavaplayer").version("1.4.1")
+    library("lavaplayer",            "com.github.MoscowMusic.lavaplayer-fork", "lavaplayer").version("1.4.11")
     library("lavaplayer-ip-rotator", "com.github.walkyst.lavaplayer-fork", "lavaplayer-ext-youtube-rotator").version("1.3.99.2")
     library("lavadsp",               "com.github.natanbc", "lavadsp").version("0.7.7")
 
